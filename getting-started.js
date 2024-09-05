2bbffba482a0463310b8891dc1e3eec043b63600
robartozh29 <robertozh29@hotmail.com>
2024-09-04 23:46:39 -0600

    Test purchase

--- getting-started.js
+++
@@ -1,9 +0,0 @@
-fixture('Getting Started')
-    .page('https://devexpress.github.io/testcafe/example');
-
-test('My first test', async t => {
-    await t
-        .typeText('#developer-name', 'John Smith')
-        .wait(5000)
-        .click('#submit-button');
-});
--- index.js
+++ index.js
@@ -20,15 +20,45 @@ test('Login', async t => {
 });
 
 test('Add Product', async t => {
+    const homeBtn = Selector('a').withText('Home').withAttribute('class', 'nav-link');
     const cartBtn = Selector('a').withText('Cart').withAttribute('class', 'nav-link');
     const prod1Link = Selector('a').withAttribute('href', 'prod.html?idp_=1');
     const addToCart = Selector('a').withText('Add to cart');
     await t
         .click(prod1Link)
-        .setNativeDialogHandler(() => true)  // 'true' indicates that you accept the alert
+        .setNativeDialogHandler(() => true)
         .click(addToCart)
         .click(cartBtn)
-        .wait(1000);
+        .wait(1000)
 });
 
-// <a href="#" onclick="addToCart(1)" class="btn btn-success btn-lg">Add to cart</a>
+const user = {
+    name: "Roberto",
+    country: "Mexico",
+    city:   "Guadalajara",
+    creditCard: "1111222233334444",
+    month: "01",
+    year: "01"
+}
+
+
+test('Purchase', async t => {
+    const cartBtn = Selector('a').withText('Cart').withAttribute('class', 'nav-link');
+    const placeOrderBtn = Selector('button').withText('Place Order').withAttribute('class', 'btn btn-success');
+    const purchaseBtn = Selector('button').withText('Purchase').withAttribute('class', 'btn btn-primary');
+    const alertBtn = Selector('button').withText('OK').withAttribute('class', 'confirm btn btn-lg btn-primary');
+    
+    await t
+        .click(cartBtn)
+        .click(placeOrderBtn)
+        .typeText('#name', user.name)
+        .typeText('#country', user.country)
+        .typeText('#city', user.city)
+        .typeText('#card', user.creditCard)
+        .typeText('#month', user.month)
+        .typeText('#year', user.year)
+        .setNativeDialogHandler(() => true)
+        .click(purchaseBtn)
+        .click(alertBtn)
+        .wait(1000);
+});

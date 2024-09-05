b0c2239cd34d6e2fea2675bfc7d05db0b285e528
robartozh29 <robertozh29@hotmail.com>
2024-09-04 23:09:49 -0600

    Test add product

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ test('Login', async t => {
         .wait(1000);   
 });
 
+test('Add Product', async t => {
+    const cartBtn = Selector('a').withText('Cart').withAttribute('class', 'nav-link');
+    const prod1Link = Selector('a').withAttribute('href', 'prod.html?idp_=1');
+    const addToCart = Selector('a').withText('Add to cart');
+    await t
+        .click(prod1Link)
+        .setNativeDialogHandler(() => true)  // 'true' indicates that you accept the alert
+        .click(addToCart)
+        .click(cartBtn)
+        .wait(1000);
+});
+
+// <a href="#" onclick="addToCart(1)" class="btn btn-success btn-lg">Add to cart</a>
